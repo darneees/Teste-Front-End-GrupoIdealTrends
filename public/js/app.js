dca4985a3ac423fa00223778d244f46bed269b1f
darneees <dgs56301@gmail.com>
2025-06-08 22:13:18 -0300

    style: criando menu mobile e eslizando sessoes

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,54 @@
 // swiper
-const swiper = new Swiper('.swiper', {
-
-  direction: 'horizontal',
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
   speed: 800,
-  effect: 'fade',
+  effect: "fade",
   fadeEffect: {
-      crossFade: true
+    crossFade: true,
   },
 
-
   breakpoints: {
-      320: {
-          slidesPerView: 1,
-          spaceBetween: 10
-      },
-      480: {
-          slidesPerView: 1,
-          spaceBetween: 10
-      },
-      640: {
-          slidesPerView: 1,
-          spaceBetween: 10
-      },
-      1024: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-      },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
   },
 
   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+});
 
+
+// menu mobile
+const menu = document.getElementById("mobile");
+const open = document.getElementById("open");
+const close = document.getElementById("closed");
+const links = document.querySelectorAll('.menu_link');
+
+open.addEventListener('click', () => {
+    menu.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('show');
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('show');
+    });
 });
